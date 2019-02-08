afcacb53182c641af53611f9d7bdba4b88e01f90
cyberspacedk <frontend.dk@gmail.com>
2019-02-08 14:21:00 +0200

    add 11 hw

---
+++ mod-11-hw/script.js
@@ -0,0 +1,165 @@
+'use strict'; 
+/*  
+  Поля объекта по которым необходимо производить фильтрацию: size, color, release_date.
+  Поля объекта для отображения в карточке: name, img, descr, color, price, release_date.
+    
+  Изначально есть форма с 3-мя секциями, состоящими из заголовка и группы 
+  чекбоксов (разметка дальше в задании). После того как пользователь выбрал 
+  какие либо чекбоксы и нажал кнопку Filter, необходимо собрать значения чекбоксов по группам. 
+  
+  🔔 Подсказка: составьте объект формата
+      const filter = { size: [], color: [], release_date: [] }
+    
+  После чего выберите из массива только те объекты, которые подходят 
+  под выбраные пользователем критерии и отрендерите список карточек товаров.
+  
+  🔔 Каждый раз когда пользователь фильтрует товары, список карточек товаров очищается, 
+      после чего в нем рендерятся новые карточки товаров, соответствующих текущим критериям фильтра.
+*/
+
+const laptops = [
+    {
+      size: 13,
+      color: 'white',
+      price: 28000,
+      release_date: 2015,
+      name: 'Macbook Air White 13"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 13,
+      color: 'gray',
+      price: 32000,
+      release_date: 2016,
+      name: 'Macbook Air Gray 13"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 13,
+      color: 'black',
+      price: 35000,
+      release_date: 2017,
+      name: 'Macbook Air Black 13"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 15,
+      color: 'white',
+      price: 45000,
+      release_date: 2015,
+      name: 'Macbook Air White 15"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 15,
+      color: 'gray',
+      price: 55000,
+      release_date: 2016,
+      name: 'Macbook Pro Gray 15"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 15,
+      color: 'black',
+      price: 45000,
+      release_date: 2017,
+      name: 'Macbook Pro Black 15"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 17,
+      color: 'white',
+      price: 65000,
+      release_date: 2015,
+      name: 'Macbook Air White 17"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 17,
+      color: 'gray',
+      price: 75000,
+      release_date: 2016,
+      name: 'Macbook Pro Gray 17"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 17,
+      color: 'black',
+      price: 80000,
+      release_date: 2017,
+      name: 'Macbook Pro Black 17"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+  ]; 
+
+const form = document.querySelector('.js-form'); 
+const output = document.querySelector('.output'); 
+
+function clearForm(){
+    output.innerHTML ='';
+}
+function buildMarkup (arr){ 
+
+const markup =  arr.reduce((acc, elem) => acc + `<div class="unit">
+    <h2>${elem.name}</h2>
+    <div class="img-wrap">
+        <img src=${elem.img}>
+    </div>
+    <div class="details">
+        <span>${elem.size}</span>
+        <span>${elem.color}</span>
+        <span>${elem.release_date}</span>
+        <span>${elem.price}</span>
+    </div>
+    <div class="description">${elem.descr}</div>
+</div> `,'');
+
+output.innerHTML = markup;
+}
+
+form.addEventListener('reset', clearForm);
+
+window.addEventListener('DOMContentLoaded', buildMarkup(laptops) );
+
+form.addEventListener('submit', (e)=>{
+
+    e.preventDefault(); 
+
+    output.innerHTML = '';
+ 
+    const arr = [...document.querySelectorAll('.js-form input:checked')];  
+
+    let size = arr.filter(elem => elem.name === 'size').map(el=> el.value);
+    let color = arr.filter(elem => elem.name === 'color').map(el=> el.value);
+    let release_date = arr.filter(elem => elem.name === 'release_date').map(el=> el.value); 
+       
+    const filter = { size, color, release_date }; 
+
+    const rezult = laptops.filter( el => (filter.size.length) ? filter.size.includes(String(el.size)) : el.size )
+                          .filter( el => (filter.color.length) ? filter.color.includes(el.color) : el.color)
+                          .filter( el=>  (filter.release_date.length) ? filter.release_date.includes(String(el.release_date)) : el.release_date); 
+
+    buildMarkup(rezult);
+     
+});
+
+//  разобраться насчет filter, что он возвращает в первом случае
+
